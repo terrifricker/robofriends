4852929ecc34cc43c93439b7efeb6e10e4cd8b22
Terri Fricker <51532684+terrifricker@users.noreply.github.com>
2023-08-15 14:28:26 -0700

    add tachyon css elements to Card element

--- src/Card.js
+++ src/Card.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 const Card = () => {
     return (
-        <>
+        <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
             <img src="https://robohash.org/test?200x200" alt="robofriend" />
             <div>
                 <h2>Robo Friend</h2>
                 <p>robofriend@gmail.com</p>
             </div>
-        </>
+        </div>
     )
 }
 
