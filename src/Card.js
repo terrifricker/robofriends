ded16592875db188d4ed704876a619673e57610c
Terri Fricker <51532684+terrifricker@users.noreply.github.com>
2023-08-15 14:49:53 -0700

    destructured props

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const Card = (props) => {
+const Card = ({id, name, email}) => {
     return (
         <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-            <img src={`https://robohash.org/${props.id}`} alt={props.name} />
+            <img src={`https://robohash.org/${id}?200x200`} alt={name} />
             <div>
-                <h2>{props.name}</h2>
-                <p>{props.email}</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     )
