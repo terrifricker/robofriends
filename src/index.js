2764ed2d71b20fd867ab1c91e42f03377f7c1d34
Terri Fricker <51532684+terrifricker@users.noreply.github.com>
2023-08-15 14:46:59 -0700

    add props to Card

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'tachyons'
 import './index.css';
+import { robots } from './robots'
 import Card from './Card';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Card />
-    <Card />
-    <Card />
+    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
+    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
+    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
   </React.StrictMode>
 );
