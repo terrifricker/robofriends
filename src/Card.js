a4aab71854236834bbee54867db9b6b015ed889a
Terri Fricker <51532684+terrifricker@users.noreply.github.com>
2023-08-15 14:23:41 -0700

    add Card component

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Card = () => {
+    return (
+        <>
+            <img src="https://robohash.org/test?200x200" alt="robofriend" />
+            <div>
+                <h2>Robo Friend</h2>
+                <p>robofriend@gmail.com</p>
+            </div>
+        </>
+    )
+}
+
+export default Card
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Card from './Card';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    </>
+    <Card />
   </React.StrictMode>
 );
