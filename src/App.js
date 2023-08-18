38bfc3f6e8cbee06bc6d33eeaa0687a4766875cd
Terri Fricker <51532684+terrifricker@users.noreply.github.com>
2023-08-18 09:52:34 -0700

    add api fetch

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import SearchBox from './SearchBox'
 import CardList from './CardList'
-import {robots} from './robots'
+
 
 class App extends React.Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+          .then(response=> response.json())
+          .then(users => {this.setState({ robots: users})});
     }
 
     onSearchChange = (event) => {
